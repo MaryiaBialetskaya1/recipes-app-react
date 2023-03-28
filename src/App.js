2675aa34013082298a3c7d27a2488d2c88801f34
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-28 19:54:31 -0400

    add testing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+import { Button } from "./components/Button";
+import { ListComponent } from "./components/ListComponent";
 
 function App() {
+  const [components, setComponents] = useState(["Mercury"]);
+  const [componentNames] = useState([
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+  ]);
+
+  function addComponent() {
+    if (componentNames.length > 0) {
+      setComponents([...components, componentNames[0]]);
+      componentNames.splice(0, 1);
+    } else {
+      window.alert("No more planets to add!");
+    }
+  }
+
   return (
-    <div className="App">
-      <h2>TEST</h2>
+    <div>
+      <Button onClick={addComponent} text="Call Component" />
+      {components.map((item, i) => (
+        <ListComponent text={item} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = (props) => {
+  return <button className="AddButton">{props.text}</button>;
+};
+
+export { Button };
