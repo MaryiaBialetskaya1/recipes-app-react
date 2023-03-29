37855b58dc25905dc4adfae3694df2255053936e
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-03-28 20:43:37 -0400

    work on  test app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import "./App.css";
 import { useState } from "react";
-import { Button } from "./components/Button";
 import { ListComponent } from "./components/ListComponent";
 
-function App() {
-  const [components, setComponents] = useState(["Mercury"]);
-  const [componentNames] = useState([
-    "Venus",
-    "Earth",
-    "Mars",
-    "Jupiter",
-    "Saturn",
-    "Uranus",
-    "Neptune",
-  ]);
+export default function App() {
+  const [isShown, setIsShown] = useState(false);
 
-  function addComponent() {
-    if (componentNames.length > 0) {
-      setComponents([...components, componentNames[0]]);
-      componentNames.splice(0, 1);
-    } else {
-      window.alert("No more planets to add!");
-    }
-  }
+  const handleClick = (event) => {
+    setIsShown((current) => !current);
+  };
 
   return (
     <div>
-      <Button onClick={addComponent} text="Call Component" />
-      {components.map((item, i) => (
-        <ListComponent text={item} />
-      ))}
+      <button onClick={handleClick}>Click</button>
+      {isShown && <ListComponent />}
     </div>
   );
 }
-
-export default App;
